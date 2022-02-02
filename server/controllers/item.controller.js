e8b6cb5a0b424b706c1d53617628d62cbbbb55a3
AymenSaadi <saadimohamedaymen2@gmail.com>
2022-02-02 20:16:48 +0100

    SignUp/login

--- server/controllers/item.controller.js
+++ server/controllers/item.controller.js
@@ -1,9 +1,10 @@
 // DELETE THIS LINE
-var selectAll = () => {};
+// var selectAll = () => {};
 
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var db = require("../database-mysql");
-// var Item = require('../database-mongo/Item.model.js');
+const bcrypt=require("bcrypt")
+var User= require('../database-mongo/Item.model.js');
 
 // UNCOMMENT IF USING MYSQL WITH CALLBACKS
 // var selectAll = function (req, res) {
@@ -16,9 +17,9 @@ var selectAll = () => {};
 //   });
 // };
 
-// UNCOMMENT IF USING MONGOOSE WITH PROMISES
+
 // var selectAll = function (req, res) {
-//   Item.find({})
+//  User.find({})
 //     .then((items) => {
 //       res.status(200).send(items);
 //     })
@@ -27,6 +28,40 @@ var selectAll = () => {};
 //     });
 // };
 
+
+    var signUp =function(req,res){
+        
+      var userData ={
+        email :req.body.email,
+        password :req.body.password,
+        name :req.body.name,
+        lastname:req.body.lastname,
+        bd :req.body.bd,
+        country :req.body.country,
+        phoneNumber :req.body.phoneNumber
+      }
+     User.create(userData,(err,data)=>{
+      if(err){
+          res.send("error")
+      } else{
+          res.send(data)
+      } 
+     }) 
+    }
+    var login =function(req,res){
+        User.findOne({"email":req.body.email},(err,user)=>{
+            if(!user)
+                res.send("user not found")
+           user.comparePassword(req.body.password,(err,isMatch)=>{
+               if(err){
+                   res.send("bad password")
+               }else{
+                   res.send(isMatch)
+               }
+           }) 
+            
+        })
+    }
 // UNCOMMENT IF USING MONGOOSE WITH PROMISES & ASYNC AWAIT
 // var selectAll = async function (req, res) {
 //   try {
@@ -37,4 +72,4 @@ var selectAll = () => {};
 //   }
 // };
 
-module.exports = { selectAll };
+module.exports = {  signUp,login};
