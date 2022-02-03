a0256a1585671ce6467f0d9011727e5e4c9a2492
sarra-benali <sarraba4@gmail.com>
2022-02-03 01:13:29 +0100

    first page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,26 +11,33 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items',
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
+
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+    return (
+    <div>
+      {/* <h1 id="p">FLYDO </h1>  */}
+      
+      <div id="sign">
+        <form  id="input" >
+          <input id="i1"  type="email" placeholder="Enter email" /> <br /> <br />
+          <input id="i2"  type="password" placeholder="Enter password"/>
+        </form>
+        <div>
+          <button id="but1">login</button>
+        </div>
+      <a href="#" id="text">Forgot Email/Password?</a>
+      </div>
+      <div>
+        <a href="#" id="text2">Create new account</a>
+      </div>
+    </div>
+    )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
